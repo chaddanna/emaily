f7d55b5b6a37bfe1df201ddb0b31b0dd6b6f3ffc
Chad Danna <chad@formulated.io>
2017-08-03 15:38:55 -0600

    finished auth flow

---
+++ services/passport.js
@@ -0,0 +1,36 @@
+const passport = require('passport')
+const GoogleStrategy = require('passport-google-oauth20').Strategy
+const mongoose = require('mongoose')
+const keys = require('../config/keys')
+
+const User = mongoose.model('users')
+
+passport.serializeUser((user, done) => done(null, user.id))
+
+passport.deserializeUser((id, done) => {
+	User.findById(id).then(user => done(null, user))
+})
+
+passport.use(
+	new GoogleStrategy(
+		{
+			clientID: keys.googleClientID,
+			clientSecret: keys.googleClientSecret,
+			callbackURL: '/auth/google/callback'
+		},
+		(accessToken, refreshToken, profile, done) => {
+			User.findOne({ googleID: profile.id }).then(existingUser => {
+				if (existingUser) {
+					// already existing record
+					// done(first arg is errors, second arg is what to return)
+					done(null, existingUser)
+				} else {
+					// no record found, make a new one
+					new User({ googleID: profile.id })
+						.save()
+						.then(user => done(null, user))
+				}
+			})
+		}
+	)
+)
