a25f71122322deda0edeb8a04ac0de13bd38d56b
Chad Danna <chad@formulated.io>
2017-08-02 21:15:03 -0600

    Talk to daron

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-	res.send({ hi: 'there' })
+	res.send({ daron: 'got a lil p' })
 })
 
 const PORT = process.env.PORT || 5000
